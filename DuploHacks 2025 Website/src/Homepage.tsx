9ae38ffca8996dd7a1f265b7ea68b0a70a4c18cd
Sehan Munir <sehanmunirgoldar@gmail.com>
2025-05-13 10:11:47 -0400

    Basic Structure
    Basic Structure

---
+++ DuploHacks 2025 Website/src/Homepage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './styles/Homepage.css';
+import Navbar from './Navbar';
+
+export default function Homepage() {
+    return (
+        <div className="homepageBody">
+            {/* Navigation Bar */}
+                <Navbar />
+            {/* Hero Section */}
+                <section className="heroSection">
+                </section>
+            {/* About Section */}
+                <section className="aboutSection">
+                </section>
+            {/* Sponsors Section */}
+                <section className="sponsorsSection">
+                </section>
+            {/* FAQ Section */}
+                <section className="faqsSection">
+                </section>
+            {/* Contact Section */}
+                <section className="contactSection">
+                </section>
+        </div>
+    );
+}
