73c3143971b4e5daa43c5c2ed22b2e157c24531e
ShaoBowen9 <bowenshao.w@gmail.com>
2025-05-18 19:23:56 -0400

    Made about into a component

--- DuploHacks 2025 Website/src/Homepage.tsx
+++ DuploHacks 2025 Website/src/Homepage.tsx
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import Sponsors from './components/sponsors';
 import Faq from './components/faq';
 import Contact from './components/contact';
+import About from './components/about';
 
 export default function Homepage() {
     return (
@@ -15,27 +16,7 @@ export default function Homepage() {
                 </section>
             {/* About Section */}
                 <section className="aboutSection">
-                    <div className="about-container">
-                        <div className="about-header-group">
-                            <div className="about-title-label">[what is duplohacks?]</div>
-                            <div className="about-title">
-                              About <span className="about-duplohacks-gradient">DuploHacks</span>
-                            </div>
-                        </div>
-                        <div className="about-content-group">
-                            <div className="about-description">
-                                <span className="about-description-bracket">&#123;</span>
-                                <span className="about-description-main"> DuploHacks is a hackathon by teenagers, for teenagers. Born out of our founders' experiences with repetitive hackathon formats, we decided to shake things up and bring something fresh to the scene.
-
-Our unique format features a main theme at kickoff, followed by a surprise second theme revealed halfway through. This challenges hackers to adapt quickly and rewards those who can connect their projects across both themes.</span>
-                                <span className="about-description-bracket">&#125;</span>
-                            </div>
-                            <div className="about-image-group">
-                                <div className="about-image-placeholder"></div>
-                                <div className="about-image-caption">located in yadda yadda yadda</div>
-                            </div>
-                        </div>
-                    </div>
+                    <About />
                 </section>
             {/* Sponsors Section */}
                 <section className="sponsorsSection">
---
+++ DuploHacks 2025 Website/src/components/about.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../styles/About.css';
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <div className="about-header-group">
+                <div className="about-title-label">[what is duplohacks?]</div>
+                <div className="about-title">
+                    About <span className="about-duplohacks-gradient">DuploHacks</span>
+                </div>
+            </div>
+
+            <div className="about-content-group">
+                <div className="about-description">
+                    <span className="about-description-bracket">&#123; </span>
+                    <span className="about-description-main">
+                        DuploHacks is a hackathon by teenagers, for teenagers. Born out of our founders' experiences
+                        with repetitive hackathon formats, we decided to shake things up and bring something fresh to
+                        the scene. Our unique format features a main theme at kickoff, followed by a surprise second
+                        theme revealed halfway through. This challenges hackers to adapt quickly and rewards those
+                        who can connect their projects across both themes.
+                    </span>
+                    <span className="about-description-bracket">&#125;</span>
+                </div>
+
+                <div className="about-image-group">
+                    <div className="about-image-placeholder"></div>
+                    <div className="about-image-caption">located in yadda yadda yadda</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default About
