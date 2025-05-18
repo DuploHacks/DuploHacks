fe7a833280686df8a5ed3b9378c4cd93a727b3e6
Hunting-Umbra <sathviksh6@gmail.com>
2025-05-18 18:47:12 -0400

    Created About DuploHacks Section

--- DuploHacks 2025 Website/src/Navbar.tsx
+++ DuploHacks 2025 Website/src/Navbar.tsx
@@ -3,7 +3,7 @@ import './styles/Navbar.css';
 export default function Navbar() {
     return (
         <div className="navBarWrapper">
-            ts pmo sybau yo gurt
+            DUPLOHACKS
         </div>
     );
 }
