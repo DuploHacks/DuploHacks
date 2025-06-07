82b26ec94ff25711d1b7c00f862b466b311da64d
ShaoBowen9 <bowenshao.w@gmail.com>
2025-06-07 15:12:08 -0400

    updated footer

--- DuploHacks 2025 Website/src/components/contact.tsx
+++ DuploHacks 2025 Website/src/components/contact.tsx
@@ -6,22 +6,26 @@ import LinkedIn from "../assets/Icons/LinkedIn.png";
 const Contact = () => {
     return (
         <>
-            <div className="footer-heading">STILL HAVE A QUESTION?</div>
-            <div className="centered-white-text">
-                REACH OUT TO US AT <span className="centered-white-bold-text"> HELLO@DUPLOHACKS.CA</span>
+            <div className="heading-container">
+                <div className="footer-heading">STILL HAVE A QUESTION?</div>
+                <div className="centered-white-text">
+                    REACH OUT TO US AT <span className="centered-white-bold-text"> HELLO@DUPLOHACKS.CA</span>
+                </div>
             </div>
+            <hr/>
+            <div className="body-container">
+                <h4 className="white-link">Code of Conduct</h4>
+                <div className="Icons">
+                    <a href="https://www.instagram.com/duplohacks_/" target="_blank" rel="noopener noreferrer">
+                        <img src={Instagram} alt="Instagram" />
+                    </a>
 
-            <div className="Icons">
-                <a href="https://www.instagram.com/duplohacks_/" target="_blank" rel="noopener noreferrer">
-                    <img src={Instagram} alt="Instagram" />
-                </a>
-
-                <a href="https://www.linkedin.com/company/duplohacks" target="_blank" rel="noopener noreferrer">
-                    <img src={LinkedIn} alt="LinkedIn" />
-                </a>
+                    <a href="https://www.linkedin.com/company/duplohacks" target="_blank" rel="noopener noreferrer">
+                        <img src={LinkedIn} alt="LinkedIn" />
+                    </a>
+                </div>
+                <h4 className="centered-white-text">© DuploHacks 2025</h4>
             </div>
-
-            <h4 className="centered-white-text">© DuploHacks 2025</h4>
         </>
     )
 }
