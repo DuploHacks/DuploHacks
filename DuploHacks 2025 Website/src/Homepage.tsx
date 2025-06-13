ac26a89349e077bdf3fe2b631421e0ba32eb6db9
Hunting-Umbra <sathviksh6@gmail.com>
2025-06-12 21:41:01 -0400

    updated sponsor us button

--- DuploHacks 2025 Website/src/Homepage.tsx
+++ DuploHacks 2025 Website/src/Homepage.tsx
@@ -48,12 +48,15 @@ export default function Homepage() {
                                 <span>Applications Coming Soon</span>
                             </div>
                         </div>
-                        <button className="sponsor-button" onClick={() => window.location.href = 'mailto:hello@duplohacks.ca'}>
+                        <a
+                            className="sponsor-button"
+                            href="mailto:HELLO@DUPLOHACKS.CA"
+                        >
                             <svg className="sponsor-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                <path d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
                             Sponsor Us?
-                        </button>
+                        </a>
                     </div>
                 </div>
             </section>
